609664c8893434380d612cac51baacb69b021985
Marte Solli Vågen <martvaag@stud.ntnu.no>
2023-04-21 11:30:15 +0200

    added view for user page

---
+++ src/views/ChooseUserView.vue
@@ -0,0 +1,13 @@
+<template>
+  <ChooseUserComponent/>
+</template>
+
+<script>
+import ChooseUserComponent from "@/components/ChooseUserComponent.vue";
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  components: {ChooseUserComponent}
+})
+
+</script>
