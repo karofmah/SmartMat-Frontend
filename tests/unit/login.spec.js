74903811b487a0e20613aa5107697603f200fca0
Marte Solli Vågen <martvaag@stud.ntnu.no>
2023-04-21 11:31:55 +0200

    added unit test file for login page

---
+++ tests/unit/login.spec.js
@@ -0,0 +1,12 @@
+import { mount } from '@vue/test-utils'
+import LoginView from '@/views/LoginView.vue'
+
+describe('Login page', () => {
+    it('renders with input when opened', () => {
+        const wrapper = mount(LoginView)
+        const emailInput = wrapper.find('#email-input')
+        const passwordInput = wrapper.find('#password-input')
+        expect(emailInput.element.textContent).toBe('')
+        expect(passwordInput.element.textContent).toBe('')
+    })
+})
