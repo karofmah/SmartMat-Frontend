62911f144292f062085f4176e4c031c5dfd76b21
Marte Solli Vågen <martvaag@stud.ntnu.no>
2023-04-21 11:30:52 +0200

    added sign out route

--- src/components/ToolbarComponent.vue
+++ src/components/ToolbarComponent.vue
@@ -10,7 +10,7 @@
       <v-tab flat color="grey" to="/list">Shopping list<v-icon icon="mdi-format-list-checkbox"></v-icon></v-tab>
       <v-tab flat color="grey" to="/recipe">Menus<v-icon icon="mdi-silverware"></v-icon></v-tab>
       <v-tab flat color="grey" to="/settings">Settings<v-icon icon="mdi-cog"></v-icon></v-tab>
-      <v-tab flat color="grey" >Sign out<v-icon icon="mdi-exit-to-app"></v-icon></v-tab>
+      <v-tab flat color="grey" to="/">Sign out<v-icon icon="mdi-exit-to-app"></v-icon></v-tab>
     </v-toolbar>
   </nav>
 </template>
