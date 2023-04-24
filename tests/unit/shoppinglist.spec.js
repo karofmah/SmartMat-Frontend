fe16e116059e82b637d720c445a258e059e96e64
Skole <jenscl@stud.ntnu.no>
2023-04-24 09:28:10 +0200

    started unit testing for the ShoppingListComponent

---
+++ tests/unit/shoppinglist.spec.js
@@ -0,0 +1,23 @@
+import { describe, it, expect } from 'vitest'
+
+import { mount } from '@vue/test-utils'
+import ShoppingListComponent from '../../src/components/ShoppingListComponent.vue'
+
+describe('ShoppingListView.vue test', () => {
+    it('initializes with correct elements', () => {
+        const wrapper = mount(ShoppingListComponent)
+        expect(wrapper.find('#shoppinglist-title').text()).toBe('Shopping list')
+        expect(wrapper.find("#input").text()).toBe('')
+        expect(wrapper.find("#shoppinglist-tooltip").text()).toBe('Add selected items to fridge')
+
+    })
+    it('removeList removes item from list when called', () => {
+        const wrapper = mount(ShoppingListComponent)
+
+
+    })
+    it('addlist adds items to list correctly when called', () => {
+        const wrapper = mount(ShoppingListComponent)
+
+    })
+})
