d7c5067411fb9e04866b4ed9a5829534da2acedb
Marte Solli Vågen <martvaag@stud.ntnu.no>
2023-04-25 09:31:35 +0200

    added method for setting a users level and deletion method

--- src/components/UserComponent.vue
+++ src/components/UserComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card variant="outlined" class="user-card" >
+  <v-card variant="outlined" class="user-card" @click="chooseUser">
     <v-card-title v-if="!edit">{{ name }} </v-card-title>
     <v-card-subtitle v-if="!edit">{{ type }}</v-card-subtitle>
     <input v-if="edit" id="edit-name-input">
@@ -9,13 +9,15 @@
     <div><img src="../assets/logo.png" id="user-image"></div>
     <v-card-actions>
       <!--LEGGE TIL AT ENDRE KNAPPEN FORVINNER PÅ STARTSIDEN-->
-      <v-btn v-if="!edit" @click="editInfo">Change info</v-btn>
-      <v-btn v-if="edit" @click="editInfo">Save info</v-btn>
+      <v-btn v-if="!edit && $route.name !== 'chooseUser'" @click="editInfo">Change info</v-btn>
+      <div v-if="edit"><v-btn @click="editInfo">Save info</v-btn></div><v-btn v-if="edit && !betaUser" @click="deleteSubuser">Delete</v-btn>
     </v-card-actions>
   </v-card>
 </template>
 
 <script>
+import settingsService from "@/services/settingsService";
+import router from "@/router"
 export default {
   name: "user-component",
   props: {
@@ -25,13 +27,40 @@ export default {
   data() {
     return {
       edit: false,
-      types: [{name: "superbruker"}, {name: "vanlig bruker"}]
+      betaUser: false,
+      types: [{name: "true"}, {name: "false"}]
     }
   },
   methods:{
+    async deleteSubuser(){
+      const subuser = {
+        "name": this.name,
+        "accessLevel": this.type,
+        "masterUser": localStorage.getItem("email")
+      }
+      console.log(subuser)
+      await settingsService.deleteSubuser(subuser)
+    },
+    async setUserLevel(){
+      if (localStorage.getItem("userType") === "false"){
+        this.betaUser = true;
+      } else {
+        this.betaUser = false;
+      }
+    },
     editInfo(){
       this.edit = !this.edit
+    },
+    chooseUser(){
+      if (this.$route.name === "chooseUser"){
+        localStorage.setItem("username", this.name)
+        localStorage.setItem("userType", this.type)
+        router.push("/fridge")
+      }
     }
+  },
+  beforeMount(){
+    this.setUserLevel()
   }
 }
 </script>
