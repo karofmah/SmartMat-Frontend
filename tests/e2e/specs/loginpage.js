166c0cb72fe4495b8ed6501ac0709691b6dfc424
Marte Solli Vågen <martvaag@stud.ntnu.no>
2023-05-02 10:24:22 +0200

    updated e2e tests with mock backend methods for login

--- tests/e2e/specs/loginpage.js
+++ tests/e2e/specs/loginpage.js
@@ -3,10 +3,43 @@
 describe('Login page tests', () => {
     beforeEach(() => {
         cy.visit('http://localhost:8000/#/')
+        cy.server();
+        cy.route({
+            method: 'POST',
+            url: 'http://localhost:8080/api/auth/authenticate',
+            response: {
+                access_token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9', // some random characters
+            },
+            status: 200
+        })
+        cy.route({
+            method: 'GET',
+            url: 'http://localhost:8080/api/refrigerators/getRefrigeratorByUser?userEmail=example@mail.com',
+            response: {
+                data: 1,
+            },
+            status: 200
+        })
+        cy.route({
+            method: 'GET',
+            url: 'http://localhost:8080/api/users/login/getUser?email=example@mail.com',
+            response: {
+                data: 1,
+            },
+            status: 200
+        })
+        cy.route({
+            method: 'POST',
+            url: 'http://localhost:8080/api/auth/register',
+            response: {
+                access_token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9', // some random characters
+            },
+            status: 201
+        })
     })
     it('Routes to user page on valid input on login', () => {
-        cy.get("#email-input").type("hei@mail.com");
-        cy.get("#password-input").type("passordhei");
+        cy.get("#email-input").type("example@mail.com");
+        cy.get("#password-input").type("password");
         cy.get('#submit-button').click();
         cy.location().should((location) => {
             expect(location.href).to.eq('http://localhost:8000/#/user')
@@ -14,21 +47,22 @@ describe('Login page tests', () => {
     })
     it('Routes to user page on valid input for register', () => {
         cy.get('#register-radiobutton').click();
-        cy.get("#email-input").type("hei@mail.com");
-        cy.get("#password-input").type("passordhei");
-        cy.get("#firstname-input").type("Marte");
-        cy.get("#lastname-input").type("Vågen");
+        cy.get("#email-input").type("example@mail.com");
+        cy.get("#password-input").type("password");
+        cy.get("#firstname-input").type("John");
+        cy.get("#lastname-input").type("Doe");
         cy.get("#phone-input").type("12345678");
         cy.get("#household-input").type("5");
         cy.get("#age-input").type("20");
+        cy.get("#pin-code-input").type("1234");
         cy.get('#submit-button').click();
         cy.location().should((location) => {
             expect(location.href).to.eq('http://localhost:8000/#/user')
         });
     })
     it('Does not route to user page on invalid input on login', () => {
-        cy.get("#email-input").type("hei");
-        cy.get("#password-input").type("passordhei");
+        cy.get("#email-input").type("example");
+        cy.get("#password-input").type("password");
         cy.get('#submit-button').click();
         cy.location().should((location) => {
             expect(location.href).to.eq('http://localhost:8000/#/')
@@ -37,7 +71,7 @@ describe('Login page tests', () => {
         cy.get("#email-input").clear();
         cy.get("#password-input").clear();
 
-        cy.get("#email-input").type("hei@mail.com");
+        cy.get("#email-input").type("example@mail.com");
         cy.get("#password-input").type("p");
         cy.get('#submit-button').click();
         cy.location().should((location) => {
@@ -46,25 +80,26 @@ describe('Login page tests', () => {
     })
     it('Does not route to user page on invalid input for register', () => {
         cy.get('#register-radiobutton').click();
-        cy.get("#email-input").type("hei@mail.com");
-        cy.get("#password-input").type("passordhei");
-        cy.get("#lastname-input").type("Vågen");
+        cy.get("#email-input").type("example@mail.com");
+        cy.get("#password-input").type("password");
+        cy.get("#lastname-input").type("Doe");
         cy.get("#phone-input").type("12345678");
         cy.get("#household-input").type("5");
         cy.get("#age-input").type("20");
+        cy.get("#pin-code-input").type("1234");
         cy.get('#submit-button').click();
         cy.location().should((location) => {
             expect(location.href).to.eq('http://localhost:8000/#/')
         });
 
         cy.get("#lastname-input").clear();
-        cy.get("#firstname-input").type("Marte");
+        cy.get("#firstname-input").type("John");
         cy.get('#submit-button').click();
         cy.location().should((location) => {
             expect(location.href).to.eq('http://localhost:8000/#/')
         });
 
-        cy.get("#lastname-input").type("Vågen");
+        cy.get("#lastname-input").type("Doe");
         cy.get("#phone-input").clear();
         cy.get('#submit-button').click();
         cy.location().should((location) => {
@@ -80,6 +115,13 @@ describe('Login page tests', () => {
 
         cy.get("#household-input").type("5");
         cy.get("#age-input").clear();
+        cy.get('#submit-button').click();
+        cy.location().should((location) => {
+            expect(location.href).to.eq('http://localhost:8000/#/')
+        });
+
+        cy.get("#age-input").type("20");
+        cy.get("#pin-code-input").clear();
         cy.get('#submit-button').click();
         cy.location().should((location) => {
             expect(location.href).to.eq('http://localhost:8000/#/')
