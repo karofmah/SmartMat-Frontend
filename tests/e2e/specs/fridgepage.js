6d9dd3cdfaa95bbb991a0bab92f9a83e807acaee
Marte Solli Vågen <martvaag@stud.ntnu.no>
2023-05-02 10:23:43 +0200

    removed test lines for code that is no longer in use

--- tests/e2e/specs/fridgepage.js
+++ tests/e2e/specs/fridgepage.js
@@ -1,10 +1,8 @@
-describe('Login page tests', () => {
+describe('Fridge page tests', () => {
     beforeEach(() => {
         cy.visit('http://localhost:8000/#/fridge')
     })
     it('allows user to generate recipe, write in item search and hover over categories', () => {
-        cy.get("#generateButton").click()
-        cy.get("#recipe").should('have.value', 'This is a test recipe')
         cy.get("#search").type('Ham')
         cy.get("#category-component").click()
     })
