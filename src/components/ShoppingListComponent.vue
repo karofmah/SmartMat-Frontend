183eb6233df7a025244e04043fe4afbbe299873a
Marte Solli Vågen <martvaag@stud.ntnu.no>
2023-05-05 11:53:36 +0200

    added snackbar and fixed methods for adding, buying and deleting items

--- src/components/ShoppingListComponent.vue
+++ src/components/ShoppingListComponent.vue
@@ -3,11 +3,11 @@
       <v-row justify="space-around">
           <v-card class="mx-auto" width="800">
               <v-toolbar color="teal">
-                  <v-toolbar-title id="shoppinglist-title">Shopping list</v-toolbar-title>
+                  <v-toolbar-title id="shoppinglist-title" class="font-weight-bold">Shopping list</v-toolbar-title>
                   <v-spacer></v-spacer>
                   <v-dialog
                     v-model="dialog"
-                    max-width="500px"
+                    max-width="600px"
                   >
                     <template v-slot:activator="{ props }">
                       <v-btn
@@ -38,6 +38,7 @@
                                   type="text"
                                   label="Food Name"
                                   clearable
+                                  :disabled="disabled"
                                   :items="allItems"
                                   v-model="editedItem.itemName"
                                   :rules="[required]"
@@ -63,7 +64,7 @@
                                 <v-select
                                   v-model="editedItem.measurementType"
                                   :items="measurementTypes"
-                                  label="Measurement type"
+                                  label="Measurement"
                                   clearable
                                   :rules="[required]"
                                 ></v-select>
@@ -97,12 +98,11 @@
                       <v-tooltip id="shoppinglist-tooltip" activator="parent" location="start">Add selected items to fridge</v-tooltip>
                   </v-btn>
               </v-toolbar>
-              <div id="lists">
+              <div id="lists" >
                   <v-data-table
                     v-model="buyItems"
                     :headers="headers"
                     :items="shoppingList"
-                    item-value="name"
                     show-select
                     class="elevation-1"
                     items-per-page="-1"
@@ -121,7 +121,7 @@
                       </v-icon>
                       <v-icon
                         size="small"
-                        @click="removeList(item.raw.name)"
+                        @click="removeList(item.raw)"
                       >
                         mdi-delete
                       </v-icon>
@@ -133,6 +133,23 @@
           </v-card>
       </v-row>
   </v-container>
+  <v-snackbar
+      v-model="snackbar"
+      color="teal"
+      :timeout="3000"
+  >
+    {{ text }}
+
+    <template v-slot:actions>
+      <v-btn
+          color="white"
+          variant="text"
+          @click="snackbar = false"
+      >
+        Close
+      </v-btn>
+    </template>
+  </v-snackbar>
 </template>
 
 <script>
@@ -142,10 +159,14 @@
   export default {
     data() {
       return {
+        text: '',
+        snackbar: false,
+        betaUser: false,
+        disabled: false,
         form: false,
         shoppingList: [],
         selectedItem: "",
-        measurementTypes: ["KG", "G", "L", "DL"],
+        measurementTypes: ["KG", "G", "L", "DL", "UNIT"],
         allItems:[],
         buyItems: [],
         headers: [
@@ -157,7 +178,7 @@
           },
           { title: 'Amount', key: 'amount' },
           { title: 'Measurement type', key: 'measurement' },
-          { title: 'Added by super user', key: 'subUser' },
+          { title: 'Added by adult', key: 'subUser' },
           { title: 'Actions', key: 'actions' },
         ],
         editedIndex: -1,
@@ -192,12 +213,14 @@
       async getShoppingList(){
         try {
           const list = await shoppingListService.getShoppingListItems(localStorage.getItem("email"))
+          localStorage.setItem("shoppingListId", list.shoppingListId)
           console.log(list.items);
           for(let i = 0; i<list.items.length; i++){
             if(!this.buyItems.includes(list.items[i].item.name)){
-              this.shoppingList.push({"name": list.items[i].item.name,"amount": list.items[i].amount, "measurement": list.items[i].measurementType, "subUser": list.items[i].subUserAccessLevel.toString()})
+              this.shoppingList.push({"name": list.items[i].item.name,"amount": list.items[i].amount, "measurement": list.items[i].measurementType, "subUser": list.items[i].subUserAccessLevel.toString(), "itemShoppingListId": list.items[i].itemShoppingListId})
             }
           }
+          console.log(this.shoppingList)
         } catch(err) {
           console.log(err)
         }
@@ -219,12 +242,20 @@
 
       },
       editItem (item) {
-        this.editedIndex = this.shoppingList.indexOf(item)
-        this.editedItem = Object.assign({}, item)
-        this.dialog = true
+        if(this.betaUser){
+          this.text = "You are not authorized to edit items"
+          this.snackbar = true
+        } else {
+          this.editedIndex = this.shoppingList.indexOf(item)
+          this.editedItem = Object.assign({}, item)
+          this.editedItem.itemName = item.name
+          this.disabled = true
+          this.dialog = true
+        }
+
       },
 
-      async deleteItem (item) {
+      /*async deleteItem (item) {
         this.editedIndex = this.shoppingList.indexOf(item)
         this.editedItem = Object.assign({}, item)
         this.shoppingList.splice(this.editedIndex, 1)
@@ -239,8 +270,9 @@
         console.log(itemToDelete)
         this.editedIndex = -1
         await shoppingListService.deleteItemFromShoppingList(itemToDelete)
-      },
+      },*/
       close () {
+        this.disabled = false
         this.dialog = false
         this.$nextTick(() => {
           this.editedItem = Object.assign({}, this.defaultItem)
@@ -256,6 +288,7 @@
           //this.shoppingList.push(this.editedItem)
         }
         this.editedItem = Object.assign({}, this.defaultItem)
+        this.disabled = false
         this.dialog = false
       },
       required (v) {
@@ -269,63 +302,76 @@
       },
 
       async buy(){
-        console.log(this.buyItems);
-         for(let i = 0; i<this.buyItems.length; i++){
-           const item = {
-             "itemName": this.buyItems[i],
-             "refrigeratorId": "1",
-             "amount": "10",
-             "measurementType": "KG"
-           }
-           await fridgeService.addNewItemToFridge(item)
+        if(this.betaUser){
+          this.text = "You are not authorized to buy items"
+          this.snackbar = true
+        } else {
+          console.log(this.buyItems);
+          for (let i = 0; i < this.buyItems.length; i++) {
+            const item = {
+              "itemName": this.buyItems[i].name,
+              "refrigeratorId": localStorage.getItem("fridgeId"),
+              "amount": this.buyItems[i].amount,
+              "measurementType": this.buyItems[i].measurement
+            }
+            await fridgeService.addNewItemToFridge(item)
 
-           const itemToDelete = {
-             "itemName": this.buyItems[i],
-             "shoppingListId": "1",
-             "amount": "10",
-             "measurementType": "KG"
-           }
-           await shoppingListService.deleteItemFromShoppingList(itemToDelete)
-           this.allItems.push(itemToDelete.itemName)
-         }
-         this.buyItems = []
-         this.shoppingList = []
-         await this.getShoppingList()
+            const itemToDelete = {
+              "itemName": this.buyItems[i].name,
+              "shoppingListId": localStorage.getItem("shoppingListId"),
+              "amount": this.buyItems[i].amount,
+              "measurementType": this.buyItems[i].measurement,
+              "itemShoppingListId": this.buyItems[i].itemShoppingListId,
+              "subUserId": localStorage.getItem("subUserId")
+            }
+            await shoppingListService.deleteItemFromShoppingList(itemToDelete)
+            //this.removeList(itemToDelete)
+            //this.allItems.push(itemToDelete.itemName)
+          }
+          this.buyItems = []
+          this.shoppingList = []
+          await this.getShoppingList()
+        }
       },
        async addItem() { //TODO: FUNKER!! (endre hardkodet)
          const itemToAdd = {
            "itemName": this.editedItem.itemName,
-           "shoppingListId": "1",
+           "shoppingListId": localStorage.getItem("shoppingListId"),
            "amount": this.editedItem.amount,
            "measurementType": this.editedItem.measurementType,
-           "itemShoppingListId": "1",
            "subUserId": localStorage.getItem("subUserId")
          }
 
          await shoppingListService.addShoppingListItems(itemToAdd)
+         this.disabled = false
          this.dialog = false
          this.shoppingList = []
          await this.getShoppingList()
        },
        async removeList(item) { //TODO: FUNKER!! (mangler hardkodet ting)
-         const itemToDelete = {
-           "itemName": item,
-           "shoppingListId": "1",
-           "amount": "1",
-           "measurementType": this.editedItem.measurementType,
-           "itemShoppingListId": "23",
-           "subUserId": localStorage.getItem("subUserId")
+         if(this.betaUser){
+           this.text = "You are not authorized to remove items"
+           this.snackbar = true
+         } else {
+           const itemToDelete = {
+             "itemName": item.name,
+             "shoppingListId": localStorage.getItem("shoppingListId"),
+             "amount": item.amount,
+             "measurementType": item.measurement,
+             "itemShoppingListId": item.itemShoppingListId,
+             "subUserId": localStorage.getItem("subUserId")
+           }
+           console.log(itemToDelete)
+           await shoppingListService.deleteItemFromShoppingList(itemToDelete)
+           this.shoppingList = []
+           await this.getShoppingList()
+           this.allItems.push(itemToDelete.itemName)
          }
-         console.log(itemToDelete)
-         await shoppingListService.deleteItemFromShoppingList(itemToDelete)
-         this.shoppingList = []
-         await this.getShoppingList()
-         this.allItems.push(itemToDelete.itemName)
        },
        async addToBuy(item) {
          const itemToBuy = {
            "itemName": item,
-           "shoppingListId": "1",
+           "shoppingListId": localStorage.getItem("shoppingListId"),
            "amount": "10",
            "measurementType": "2"
          }
@@ -335,7 +381,7 @@
        async removeFromBuy(item) {
          const itemFromBuy = {
            "itemName": item,
-           "shoppingListId": "1",
+           "shoppingListId": localStorage.getItem("shoppingListId"),
            "amount": "10",
            "measurementType": "2"
          }
@@ -345,10 +391,14 @@
       async mount() {
         await this.getShoppingList()
         await this.getAllItems()
+      },
+      async setUserLevel(){
+        this.betaUser = localStorage.getItem("userType") === "false";
       }
     },
     beforeMount(){
       this.mount()
+      this.setUserLevel()
     },
     mounted(){
       if (localStorage.getItem("token") === null){
